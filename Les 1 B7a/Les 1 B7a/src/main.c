42a221999c3af12bee3183ae5b7b43210287cfc1
Unknown <maartenwn@gmail.com>
2018-02-06 13:47:08 +0100

    Fixed B7a

--- Les 1 B7a/Les 1 B7a/src/main.c
+++ Les 1 B7a/Les 1 B7a/src/main.c
@@ -5,7 +5,6 @@
 **
 ** main.c
 **
-** Beschrijving:	Flash PORTD.7 when PORTC.0 is pressed
 ** Target:			AVR mcu
 ** Author: 			maartenwn@gmail.com en aaron.israëls@home.nl
 ** -------------------------------------------------------------------------*/
@@ -16,8 +15,8 @@
 #include <stdio.h>
 
 
-typedef enum {D5,D6,D7} EVENTS;
-typedef enum {Start,S1,S2,S3,End} STATES;
+typedef enum {UNKOWN,D5,D6,D7} EVENTS;
+typedef enum {UNKOWNSTATE,Start,S1,S2,S3,End} STATES;
 
 typedef struct {
 	STATES next;
@@ -36,19 +35,19 @@ void handleD7(STATE *currentState);
 
 STATE states[5] = {
 	{
-		Start , { {S1,D7} ,{S1,D6} , {S2,D5} }
+		Start , { {S1,D6} , {S2,D5} , {UNKOWN,UNKOWNSTATE} }
 	},
 	{
-		S1 , { {S2,D5} }
+		S1 , { {S2,D5} , {Start,D7}, {UNKOWN,UNKOWNSTATE} }
 	},
 	{
-		S2, { {Start ,D6} , {S3,D5} }
+		S2, { {Start ,D7} , {S3,D5} , {UNKOWN,UNKOWNSTATE} }
 	},
 	{
 		S3 ,{ {Start , D7}, {End,D5} , {End , D6} }
 	},
 	{
-		End , { {Start, D7} }
+		End , { {Start, D7} , {UNKOWN,UNKOWNSTATE}, {UNKOWN,UNKOWNSTATE} }
 	}
 };
 
@@ -77,6 +76,7 @@ void handleD5(STATE *currentState){
 	for(index = 0; index < 3; index++){
 		if(currentState->events[index].trigger == D5){
 			int i = 0;
+			EVENTS *next = &currentState->events[index].trigger;
 			for(i = 0; i < 5; i++){
 				if(currentState->events[index].next == states[i].state){
 					*currentState = states[i];
@@ -119,7 +119,7 @@ void handleD7(STATE *currentState){
 
 
 /******************************************************************]
-short:			main() loop, entry point of executable
+short:			main() loop, entry point of executable https://youtu.be/YS4TXIHeLDU
 inputs:			
 outputs:	
 notes:			Looping forever
@@ -127,21 +127,38 @@ Version :    	DMK, Initial code
 *******************************************************************/
 int main( void )
 {
-	DDRD = 0b11111111;			// All pins PORTD are set to output
-
+	//DDRD = 0b11111111;			// All pins PORTD are set to output
+	DDRE = 0b11111111;	
 
 	STATE currentState = states[0];
 
-	handleD7(&currentState); 
 	handleD5(&currentState);
-	handleD5(&currentState);
-	handleD6(&currentState);
+	
 
 	
 	while (1)
 	{
+		if(PIND & 32){
+			handleD5(&currentState);
+		}else if(PIND & 64){
+			handleD6(&currentState);
+		}else if(PIND & 128){
+			handleD7(&currentState);
+		}
+
+		
+
+
 		if(currentState.state == states[4].state){
-			PORTD = 0xFF;
+			PORTE = 0xFF;
+		}else if(currentState.state == states[0].state){
+			PORTE = 0x00;
+		}else if(currentState.state == states[1].state){
+			PORTE =0x01;
+		}else if(currentState.state == states[2].state){
+			PORTE = 0x02;
+		}else if(currentState.state == states[3].state){
+			PORTE = 0x04;
 		}
 		wait(100);		
 	}
